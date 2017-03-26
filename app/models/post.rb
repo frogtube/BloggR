09f2a416e99e2ce9069f30ebdb31ab552e9912ea
frogtube <u.pradere@gmail.com>
2017-03-26 13:24:53 +0200

    Migration banner_image_url and configuration

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,8 @@ class Post < ApplicationRecord
   def should_generate_new_friendly_id?
     title_changed?
   end
+
+  def published_at
+    "Published #{created_at.strftime('%-b %-d, %Y')}"
+  end
 end
