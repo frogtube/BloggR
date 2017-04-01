0a695811fbccd8f38b51d6599fa76ee50bc18c77
frogtube <u.pradere@gmail.com>
2017-04-01 19:02:33 +0200

    Some navbar restyling

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get '/author', to: 'posts#author', as: :author
   put 'publish', to: 'posts#publish'
   put 'unpublish', to: 'posts#unpublish'
+
+  get '/account', to: 'accounts#edit', as: :account
   
 end
